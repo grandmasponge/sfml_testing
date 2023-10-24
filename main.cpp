13e7ea595f25b4175974a88ed91645f57ecc3994
grandmasponge <tobyr2006@gmail.com>
2023-10-24 21:29:27 +0100

    commit 2 going strong

--- main.cpp
+++ main.cpp
@@ -95,11 +95,54 @@ class Bullet {
             this->mesh.setFillColor(sf::Color::Blue);
         }
         sf::CircleShape mesh;
-        float max_vel = 5.f;
+        sf::Int32 prev = sf::Time::Zero.asMilliseconds();
+        float max_vel = 6.f;
 };
 
+class Dummy {
+    public:
+        sf::Vector2f position;
+        sf::RectangleShape mesh = sf::RectangleShape(sf::Vector2f(30, 30));
+        Dummy(sf::Vector2f position) {
+            this->position = position;
+            this->mesh.setPosition(position);
+            this->mesh.setFillColor(sf::Color::Green);
+        }
+        void Collision(Bullet *bullet)
+        {
+            if(this->mesh.getGlobalBounds().intersects(bullet->mesh.getGlobalBounds()))
+            {
+                std::cout << "hit" << std::endl;
+            }
+        }
+};
 
+Player player(sf::Vector2f(100, 100), sf::Vector2f(0, 0), sf::Vector2f(0, 0));
 std::vector<Bullet> bullets;
+Bullet bullet;
+sf::Clock delta_clock;
+
+void shooting()
+{
+    if(sf::Mouse::isButtonPressed(sf::Mouse::Left) && delta_clock.getElapsedTime().asMilliseconds() - bullet.prev > 100)
+    {
+        bullet.mesh.setPosition(player.mesh.getPosition());
+        bullet.current_vel = player.Aimdir * bullet.max_vel;
+        bullets.push_back(bullet);
+        bullet.prev = delta_clock.getElapsedTime().asMilliseconds();
+    }
+}
+
+void cleanup()
+{
+    for(size_t i = 0; i < bullets.size(); i++)
+    {
+        if(bullets[i].mesh.getPosition().x > 800 || bullets[i].mesh.getPosition().x < 0 || bullets[i].mesh.getPosition().y > 600 || bullets[i].mesh.getPosition().y < 0)
+        {
+            bullets.erase(bullets.begin() + i);
+        }
+    }
+}
 
 int main()
 {
@@ -107,8 +150,6 @@ int main()
     settings.antialiasingLevel = 8;
     sf::RenderWindow window(sf::VideoMode(800, 600), "Test", sf::Style::Default, settings);
     window.setVerticalSyncEnabled(true);
-    
-    Player player(sf::Vector2f(100, 100), sf::Vector2f(0, 0), sf::Vector2f(0, 0));
     
     while (window.isOpen())
     {
@@ -122,34 +163,17 @@ int main()
         }
         player.update(sf::Vector2f(sf::Mouse::getPosition(window)));
         
-        //shooting
-        Bullet bullet;
-        if(sf::Mouse::isButtonPressed(sf::Mouse::Left))
-        {
-            
-            bullet.mesh.setPosition(player.position);
-            bullet.current_vel =  player.Aimdir * bullet.max_vel;
-            std::cout << bullet.current_vel.x << " " << bullet.current_vel.y << std::endl;
-            bullets.push_back(Bullet(bullet));
-        }
-
-        // window.draw(bullet.mesh)
-        
         window.draw(player.mesh);
 
         for (size_t i = 0; i < bullets.size(); i++)
         {
             bullets[i].mesh.move(bullets[i].current_vel);
-            std::cout << bullets.size() << std::endl;
+            
             window.draw(bullets[i].mesh);
             bullets[i].mesh.move(bullets[i].current_vel);
         }
-
-        //undrawbullets
-        for (size_t i = 0; i < bullets.size(); i++)
-        {
-            
-        }
+        shooting();
+        cleanup();
         window.display();
     }
 
